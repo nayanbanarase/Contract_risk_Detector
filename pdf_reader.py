85272924ab503bac7b291dfa0d27a021865bb2db
Nayan Banarase <nayanbanarase@gmail.com>
2026-07-02 22:10:35 +0530

    Create pdf_reader.py

---
+++ pdf_reader.py
@@ -0,0 +1,12 @@
+from PyPDF2 import PdfReader
+
+def extract_text(path):
+
+    reader = PdfReader(path)
+
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text()
+
+    return text
